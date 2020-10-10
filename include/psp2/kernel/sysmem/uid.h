1c545b76c801353eea86cc02df8a1abfe90e94cf
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-10-10 15:03:17 -0400

    Add uid.h

---
+++ include/psp2/kernel/sysmem/uid.h
@@ -0,0 +1,15 @@
+#ifndef _DOLCESDK_PSP2_KERNEL_SYSMEM_UID_H_
+#define _DOLCESDK_PSP2_KERNEL_SYSMEM_UID_H_
+
+#include <psp2/cdefs.h>
+
+SCE_CDECL_BEGIN
+
+/**
+ * UID is created by sceKernelOpen***()
+ */
+#define SCE_UID_ATTR_OPEN_FLAG			0x00080000U
+
+SCE_CDECL_END
+
+#endif  /* _DOLCESDK_PSP2_KERNEL_SYSMEM_UID_H_ */
