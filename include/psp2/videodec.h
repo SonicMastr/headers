9c416a5e14097d9889799f77fdd07b8c61148360
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2020-10-14 21:29:40 -0400

    Add internal and nongame functions for videodec.h

--- include/psp2/videodec.h
+++ include/psp2/videodec.h
@@ -1,75 +1,103 @@
-/**
- * \usergroup{SceVideoDec}
- * \usage{psp2/videodec.h,SceVideodec_stub,SCE_SYSMODULE_AVCDEC}
- */
-
-
-#ifndef _PSP2_VIDEODEC_H_
-#define _PSP2_VIDEODEC_H_
+#ifndef _DOLCESDK_PSP2_VIDEODEC_H_
+#define _DOLCESDK_PSP2_VIDEODEC_H_
 
 #include <psp2/types.h>
-#include <stdint.h>
 
-#ifdef __cplusplus
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
 extern "C" {
-#endif
+#endif /* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
 
-typedef enum SceVideodecErrorCode {
-	SCE_VIDEODEC_ERROR_INVALID_TYPE                 = (int)0x80620801,
-	SCE_VIDEODEC_ERROR_INVALID_PARAM                = (int)0x80620802,
-	SCE_VIDEODEC_ERROR_OUT_OF_MEMORY                = (int)0x80620803,
-	SCE_VIDEODEC_ERROR_INVALID_STATE                = (int)0x80620804,
-	SCE_VIDEODEC_ERROR_UNSUPPORT_IMAGE_SIZE         = (int)0x80620805,
-	SCE_VIDEODEC_ERROR_INVALID_COLOR_FORMAT         = (int)0x80620806,
-	SCE_VIDEODEC_ERROR_NOT_PHY_CONTINUOUS_MEMORY    = (int)0x80620807,
-	SCE_VIDEODEC_ERROR_ALREADY_USED                 = (int)0x80620808,
-	SCE_VIDEODEC_ERROR_INVALID_POINTER              = (int)0x80620809,
-	SCE_VIDEODEC_ERROR_ES_BUFFER_FULL               = (int)0x8062080A,
-	SCE_VIDEODEC_ERROR_INITIALIZE                   = (int)0x8062080B,
-	SCE_VIDEODEC_ERROR_NOT_INITIALIZE               = (int)0x8062080C,
-	SCE_VIDEODEC_ERROR_INVALID_STREAM               = (int)0x8062080D,
-	SCE_VIDEODEC_ERROR_INVALID_ARGUMENT_SIZE        = (int)0x8062080E
-} SceVideodecErrorCode;
 
-typedef enum SceVideodecType {
-	SCE_VIDEODEC_TYPE_HW_AVCDEC = 0x1001
-} SceVideodecType;
+#define SCE_VIDEODEC_ERROR_INVALID_TYPE					(-2141059071) /* 0x80620801 */
+#define SCE_VIDEODEC_ERROR_INVALID_PARAM				(-2141059070) /* 0x80620802 */
+#define SCE_VIDEODEC_ERROR_OUT_OF_MEMORY				(-2141059069) /* 0x80620803 */
+#define SCE_VIDEODEC_ERROR_INVALID_STATE				(-2141059068) /* 0x80620804 */
+#define SCE_VIDEODEC_ERROR_UNSUPPORT_IMAGE_SIZE			(-2141059067) /* 0x80620805 */
+#define SCE_VIDEODEC_ERROR_INVALID_COLOR_FORMAT			(-2141059066) /* 0x80620806 */
+#define SCE_VIDEODEC_ERROR_NOT_PHY_CONTINUOUS_MEMORY	(-2141059065) /* 0x80620807 */
+#define SCE_VIDEODEC_ERROR_ALREADY_USED					(-2141059064) /* 0x80620808 */
+#define SCE_VIDEODEC_ERROR_INVALID_POINTER				(-2141059063) /* 0x80620809 */
+#define SCE_VIDEODEC_ERROR_ES_BUFFER_FULL				(-2141059062) /* 0x8062080A */
+#define SCE_VIDEODEC_ERROR_INITIALIZE					(-2141059061) /* 0x8062080B */
+#define SCE_VIDEODEC_ERROR_NOT_INITIALIZE				(-2141059060) /* 0x8062080C */
+#define SCE_VIDEODEC_ERROR_INVALID_STREAM				(-2141059059) /* 0x8062080D */
+#define SCE_VIDEODEC_ERROR_INVALID_ARGUMENT_SIZE		(-2141059058) /* 0x8062080E */
 
-typedef struct SceVideodecQueryInitInfoHwAvcdec {
-	uint32_t size;				//!< sizeof(SceVideodecQueryInitInfoHwAvcdec)
-	uint32_t horizontal;
-	uint32_t vertical;
-	uint32_t numOfRefFrames;    //!< Number of reference frames to use
-	uint32_t numOfStreams;      //!< Must be set to 1
+
+/* decoder init type */
+#define SCE_VIDEODEC_TYPE_HW_AVCDEC	(0x1001U)
+#define SCE_VIDEODEC_TYPE_HW_MPEG4	(0x1002U)
+
+typedef struct  SceVideodecQueryInitInfoHwAvcdec {
+	SceSize		size;
+	SceUInt32	horizontal;
+	SceUInt32	vertical;
+	SceUInt32	numOfRefFrames;
+	SceUInt32	numOfStreams;
 } SceVideodecQueryInitInfoHwAvcdec;
 
-typedef union SceVideodecQueryInitInfo {
-	uint8_t                           reserved[32];
-	SceVideodecQueryInitInfoHwAvcdec  hwAvc;
+
+
+typedef union SceVideodecQueryInitInfo  {
+	SceUInt8                    reserved[32];
+	SceVideodecQueryInitInfoHwAvcdec		hwAvc;
 } SceVideodecQueryInitInfo;
 
+
+typedef struct SceVideodecMemInfo {
+	SceUInt32	memSize;
+
+} SceVideodecMemInfo;
+
+
+typedef struct SceVideodecBuf {
+	void		*pBuf;
+	SceUInt32	size;
+} SceVideodecBuf;
+
+typedef	struct SceVideodecCtrl {
+	SceVideodecBuf	memBuf;
+	SceUID			memBufUid;
+
+	SceUIntVAddr	vaContext;
+	SceUInt32		contextSize;
+} SceVideodecCtrl;
+
+extern SceInt32 sceVideodecSetConfig(SceUInt32 codecType, SceInt32 config);
+
+extern SceInt32 sceVideodecInitLibrary(SceUInt32 codecType, const SceVideodecQueryInitInfo *pInitInfo);
+extern SceInt32 sceVideodecQueryMemSize(SceUInt32 codecType, const SceVideodecQueryInitInfo *pInitInfo, SceVideodecMemInfo *pMemInfo);
+extern SceInt32 sceVideodecInitLibraryWithUnmapMem(SceUInt32 codecType, SceVideodecCtrl *pCtrl, const SceVideodecQueryInitInfo *pInitInfo);
+extern SceInt32 sceVideodecTermLibrary(SceUInt32 codecType);
+
+/* Internal */
+
+extern SceInt32 sceVideodecSetConfigInternal(SceUInt32 codecType, SceInt32 config);
+
+extern SceInt32 sceVideodecQueryMemSizeInternal(SceUInt32 codecType, const SceVideodecQueryInitInfo *pInitInfo, SceVideodecMemInfo *pMemInfo);
+extern SceInt32 sceVideodecInitLibraryInternal(SceUInt32 codecType, const SceVideodecQueryInitInfoHwAvcdec *pInitInfo);
+extern SceInt32 sceVideodecInitLibraryWithUnmapMemInternal(SceUInt32 codecType, SceVideodecCtrl *pCtrl, const SceVideodecQueryInitInfo *pInitInfo);
+
+/* Nongame */
+
+extern SceInt32 sceVideodecQueryMemSizeNongameapp(SceUInt32 codecType, const SceVideodecQueryInitInfo *pInitInfo, SceVideodecMemInfo *pMemInfo);
+extern SceInt32 sceVideodecInitLibraryNongameapp(SceUInt32 codecType, const SceVideodecQueryInitInfoHwAvcdec *pInitInfo);
+extern SceInt32 sceVideodecInitLibraryWithUnmapMemNongameapp(SceUInt32 codecType, SceVideodecCtrl *pCtrl, const SceVideodecQueryInitInfo *pInitInfo);
+
+#define SCE_VIDEODEC_VOID_TIMESTAMP (0xffffffff)
+#define SCE_VIDEODEC_PARAM_UNKNOWN  (0xffffffff)
+
 typedef struct SceVideodecTimeStamp {
-	uint32_t upper;
-	uint32_t lower;
+	SceUInt32	upper;
+	SceUInt32	lower;
 } SceVideodecTimeStamp;
 
-/**
- * @param[in] codec - See ::SceVideodecType
- * @param[in] initInfo - See ::SceVideodecQueryInitInfoHwAvcdec
- *
- * @return 0 on success, < 0 on error.
- */
-int sceVideodecInitLibrary(SceVideodecType codec, const SceVideodecQueryInitInfoHwAvcdec *initInfo);
 
-/**
- * @param[in] codec - See ::SceVideodecType
- *
- * @return 0 on success, < 0 on error.
- */
-int sceVideodecTermLibrary(SceVideodecType codec);
-
-#ifdef __cplusplus
+#if defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus)
 }
-#endif
+#endif /* defined(_LANGUAGE_C_PLUS_PLUS)||defined(__cplusplus)||defined(c_plusplus) */
 
-#endif /* _PSP2_VIDEODEC_H_ */
+#include <psp2/avcdec.h>
+
+#endif /* _DOLCESDK_PSP2_VIDEODEC_H_ */
+
